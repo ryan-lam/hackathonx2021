39bd83adee2230d2c498910d3d78ef931abfa271
ryan-lam <ryan-lam@users.noreply.github.com>
2021-06-25 18:51:34 -0400

    migrations

--- project/backend/models.py
+++ project/backend/models.py
@@ -3,24 +3,24 @@ from django.db import models
 # Create your models here.
 
 class User(models.Model):
-    username = models.CharField(max_length="64", unique=True)
-    password = models.CharField(max_length="64")
-    name = models.CharField(max_length="64")
+    username = models.CharField(max_length=64, unique=True)
+    password = models.CharField(max_length=64)
+    name = models.CharField(max_length=64)
 
     def __str__(self):
         return f'{self.name}. Username: {self.username}'
 
 class Item(models.Model):
-    name = models.CharField(max_length="200")
-    category = models.CharField(max_length="200", null=True)
-    description = models.CharField(max_length="3000")
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200, null=True)
+    description = models.CharField(max_length=3000)
     img = models.ImageField()
 
     def __str__(self):
         return f'id: {self.id}, name: {self.name}, category: {self.category}'
 
 class Course(models.Model):
-    code = models.CharField(max_length="7", unique=True)
+    code = models.CharField(max_length=7, unique=True)
     item = models.ManyToManyField(Item, related_name='courses')
 
     def __str__(self):
--- project/backend/urls.py
+++ project/backend/urls.py
@@ -1,1 +1,7 @@
 from . import views 
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', views.index, name="index"),
+]
