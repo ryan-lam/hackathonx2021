9c78467956d40029a1f0bca17dc8f844c8099763
Jaxz10 <42484410+Jaxz102@users.noreply.github.com>
2021-06-25 18:46:48 -0400

    Frotend

--- project/backend/urls.py
+++ project/backend/urls.py
@@ -1,1 +1,9 @@
-from . import views 
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    # path("repdashboard", views.repdashboard, name="repdashboard"),
+    # path("clientdashboard", views.clientdashboard, name="clientdashboard"),
+    # path("booking", views.booking, name="booking")
+]
