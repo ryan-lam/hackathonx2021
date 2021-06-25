6793a16691e93940b995b8eb99c2b4c2762780da
michaelng0107 <33400111+michaelng0107@users.noreply.github.com>
2021-06-25 18:17:54 -0400

    created initial models

--- project/backend/models.py
+++ project/backend/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    id_ = models.CharField(max_length="200", required=True)
+    name = models.CharField(max_length="200")
+    art_type = models.CharField(max_length="200")
+    img = models.ImageField()
+
+class Course(models.Model):
+    code = models.OneToOneField()
+    sequence = models.ExpressionList
