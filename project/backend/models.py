0ac97d33985f8928e3ce38cce3d8cda79e96ceb2
michaelng0107 <33400111+michaelng0107@users.noreply.github.com>
2021-06-25 18:46:03 -0400

    updated models.py

--- project/backend/models.py
+++ project/backend/models.py
@@ -7,15 +7,25 @@ class User(models.Model):
     password = models.CharField(max_length="64")
     name = models.CharField(max_length="64")
 
+    def __str__(self):
+        return f'{self.name}. Username: {self.username}'
+
 class Item(models.Model):
     name = models.CharField(max_length="200")
     category = models.CharField(max_length="200", null=True)
     description = models.CharField(max_length="3000")
     img = models.ImageField()
 
+    def __str__(self):
+        return f'id: {self.id}, name: {self.name}, category: {self.category}'
+
 class Course(models.Model):
-    code = models.OneToOneField()
-    sequence = models.ExpressionList
+    code = models.CharField(max_length="7", unique=True)
+    item = models.ManyToManyField(Item, related_name='courses')
+
+    def __str__(self):
+        items = [str(item) for item in self.item.all()].join('\n')
+        return f'Code: {self.code} \n, {items}'
 
 
 
