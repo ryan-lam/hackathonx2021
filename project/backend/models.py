669acae5b211f1404cdd9a8c39c945c1d7a68930
ryan-lam <ryan-lam@users.noreply.github.com>
2021-06-25 18:23:39 -0400

    models

--- project/backend/models.py
+++ project/backend/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class user(models.Model):
+class User(models.Model):
     username = models.CharField(max_length="64", unique=True)
     password = models.CharField(max_length="64")
     name = models.CharField(max_length="64")
