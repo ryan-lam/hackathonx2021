c20bc067b721637f0c559fc1c6174fa65583dc05
ryan-lam <ryan-lam@users.noreply.github.com>
2021-06-26 15:48:40 -0400

    changed admin route

--- project/project/urls.py
+++ project/project/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('duckduckgoose/', admin.site.urls),
     path('', include('backend.urls')),
 ]
